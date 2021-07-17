ea2eea0dc497b40fc1c44e8c06d9ea60cde85158
Rob G <popestarkiller@icloud.com>
2021-07-17 12:40:18 -0700

    flask 98% complete, tried to js file

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_pymongo import PyMongo
 import stonks
 from flask import send_from_directory
 import os
-
+import json
+from bson import json_util
 
 # initialize flask
 app = Flask(__name__)
@@ -13,28 +14,17 @@ mongo = PyMongo(app, uri='mongodb://localhost:27017/stonks')
 
 @app.route("/")
 def home():
-	# grab data from mongo database
+	stonks_data = stonks.api_call()
+	mongo.db.collection.update({}, stonks_data, upsert=True)	
 	api_data = mongo.db.collection.find_one()
-
-	# return template and data
 	return render_template("index.html", api_data = api_data)
 
 
-@app.route("/api")
-def api_information():
-	# run scrape function
-	stonks_data = stonks.api_call()
 
-	# update Mongo database 
-	mongo.db.collection.update({}, stonks_data, upsert=True)
-
-	# return redirect to home page
-	return redirect("/")
-
-@app.route('/js/<path:filename>')
-def serve_static(filename):
-    root_dir = os.path.dirname(os.getcwd())
-    return send_from_directory(os.path.join(root_dir, 'static',), filename)
+@app.route("/data")
+def stock_data():
+    stock_data = mongo.db.collection.find_one()
+    return json.loads(json_util.dumps(stock_data))
 
 if __name__ == "__main__":
 	app.run(debug=True)	
